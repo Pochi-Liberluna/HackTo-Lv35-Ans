a56e87b25dced55439f47bc563ec43ecffd194f3
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-04-15 00:55:00 +0900

    Create Lv35 Ans-Shell.c

---
+++ Lv35 Ans-Shell.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    char filename[] = "/bin/sh\x00";
+    char *argv[1];
+    char *envp[1];
+
+    argv[0] = "/bin/sh";
+    argv[1] = 0;
+
+    envp[0] = 0;
+
+    execve(filename, argv, envp);
+}
